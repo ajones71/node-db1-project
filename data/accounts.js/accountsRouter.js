ca98a999d704fc21470ec02cb4b44bf9bc8b80dc
Antoinette Jones <aajones71@gmail.com>
2020-09-18 21:13:03 -0400

    everything looks like it's working

--- data/accounts.js/accountsRouter.js
+++ data/accounts.js/accountsRouter.js
@@ -25,15 +25,46 @@ router.get("/:id", async (req, res)=> {
     }
 })
 
-router.post("/", (req, res) =>{
-
+router.post("/", async (req, res) =>{
+    const newAccount = req.body;
+try {
+    const sql = db("accounts").insert(newAccount).toString();
+    console.log(sql)
+    const account = await  db("accounts").insert(newAccount);
+    res.status(201).json(account)
+}catch(err){
+    res.status(500).json({message: "Error inserting account", error:err})
+}
 })
 
-router.put("/:id", (req, res) => {
-
+router.put("/:id", async(req, res) => {
+    const{id} = req.params;
+    const changes = req.body
+try{
+    const count = await db("accounts").update(changes).where({id})
+    if(count){
+        res.json({update: count});
+    }else{
+        res.status(404).json({message: "Invalid ID"})
+    }
+}
+catch{
+    res.status(500).json({message: "Error updating account"})
+}
 })
 
-router.delete("/:id", (req, res)=> {
-
+router.delete("/:id", async(req, res)=> {
+    const {id} = req.params;
+try{
+    const count = await db("accounts").where({id}).del();
+    if(count){
+        res.json({deleted: count});
+    }else{
+        res.status(404).json({message: "Invalid ID"})
+    }
+}
+catch(err){
+ res.status(500).json({message: "Error deleting record", error:err})      
+}
 })
 module.exports = router;
